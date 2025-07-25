5f14e7977ff9fcad66c56d35dece171ded9fcdf4
keremzor <work@keremzor.com>
2025-07-25 23:53:38 +0300

    fix: cleanup timers in TextSwitcher

--- src/components/TextSwitcher/TextSwitcher.tsx
+++ src/components/TextSwitcher/TextSwitcher.tsx
@@ -10,16 +10,16 @@ export function TextSwitcher({ config }: { config: TextSwitcherConfig }) {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
+    let timer: NodeJS.Timeout;
     const delay = setTimeout(() => {
-      const timer = setInterval(() => {
+      timer = setInterval(() => {
         setIndex((prev) => (prev + 1) % config.texts.length);
       }, config.interval);
-      return () => {
-        clearTimeout(delay);
-        clearInterval(timer);
-      };
     }, 1000);
-    return () => clearTimeout(delay);
+    return () => {
+      clearTimeout(delay);
+      clearInterval(timer);
+    };
   }, [config.interval, config.texts.length]);
 
   return <h1 className={`${styles.switcher} ff-700 fs-32 text-white`}>{config.texts[index]}</h1>;
